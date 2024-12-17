36324880440b5baaa3cb2a1f5bbc2f40e4e12e50
Nikita Dubinin <ndubinin2002@gmail.com>
2024-12-17 15:20:47 +0200

    React__Homework__7

--- start_materials/documentation-site/src/index.js
+++ start_materials/documentation-site/src/index.js
@@ -62,7 +62,6 @@ root.render(
 async function loader({params}) {
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
     const user = await res.json();
-    console.log(user);
     return user;
 }
 
